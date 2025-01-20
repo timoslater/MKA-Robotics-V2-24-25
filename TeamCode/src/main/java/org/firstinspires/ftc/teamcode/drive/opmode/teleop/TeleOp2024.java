f74f4e713c537a4b177b17e07a262e443d806d3f
timoslater <tsla0307@gmail.com>
2025-01-20 12:57:35 -0500

    Implement Motor Controller for Teleop Control

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/teleop/TeleOp2024.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/teleop/TeleOp2024.java
@@ -1,6 +1,9 @@
 package org.firstinspires.ftc.teamcode.drive.opmode.teleop;
 
 
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
@@ -9,9 +12,10 @@ import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.IMU;
 import com.qualcomm.robotcore.hardware.Servo;
 
-import org.firstinspires.ftc.teamcode.drive.opmode.teleop.utils.AsyncArmActions;
+import org.firstinspires.ftc.teamcode.utils.MotorController;
+import org.firstinspires.ftc.teamcode.utils.PIDController;
 
-
+@Config
 @TeleOp(name = "MAIN", group = "Linear Opmode")
 public class TeleOp2024 extends LinearOpMode {
 
@@ -35,12 +39,18 @@ public class TeleOp2024 extends LinearOpMode {
     private Servo elbow;
     private int lastPos = 325;
     private boolean isDropping = false;
+    private int rotateIndex;
+    private double[] rotatePositions = {.055,.222, .555,.888};
+
+    private MotorController liftController;
+    public static double p = 0, i = 0, d = 0;
+    public static double f = 0;
+    public static int target = 0;
+    private final double ticks = 384.5;
 
     public boolean liftRunning = false;
-
-    private final double minLiftPower = 0.25;
-
-    //public boolean positionSet = false;
+    public boolean positionSet = false;
+    private double minLiftPower = 0.25;
 
     public void movement() {
         double modifier = 1;//nearBoard ? 0.65 : 1;
@@ -62,7 +72,7 @@ public class TeleOp2024 extends LinearOpMode {
         leftRear.setPower(v3);
         rightRear.setPower(v4);
     }
-    
+
     public void liftUp(double power) {
         lift1.setPower(power);
         lift2.setPower(power);
@@ -86,19 +96,19 @@ public class TeleOp2024 extends LinearOpMode {
 
     public void clawOpen() {
         claw.setPosition(.3);
-//        if (!isDropping) {
-//            elbow.setPosition(0.296);
-//            hand.setPosition(0.92);
-//        }
+        if (!isDropping) {
+            elbow.setPosition(0.296);
+            hand.setPosition(0.416);
+        }
 
     }
 
     public void clawClose() {
         claw.setPosition(.64);
-//        if (!isDropping) {
-//            elbow.setPosition(0.38);
-//            hand.setPosition(0.94);
-//        };
+        if (!isDropping) {
+            elbow.setPosition(0.38);
+            hand.setPosition(0.5);
+        };
     }
 
     public void claw2Open(){
@@ -109,28 +119,39 @@ public class TeleOp2024 extends LinearOpMode {
     }
 
     public void rotateClawR() {
-        rotate.setPosition(rotate.getPosition()+.01);
+
+        if(rotateIndex<rotatePositions.length-1) {
+            rotateIndex++;
+            rotate.setPosition(rotatePositions[rotateIndex]);
+        }
+
+
     }
     public void rotateClawL() {
-        rotate.setPosition(rotate.getPosition()-.01);
+        if(rotateIndex>0) {
+            rotateIndex--;
+            rotate.setPosition(rotatePositions[rotateIndex]);
+        }
+
     }
 
-//    public void armPositionIdle() {
-//        isDropping = false;
-//        hand.setPosition(.92);
-//        clawOpen();
-//    }
-//
-//    public void armPositionDrop() throws InterruptedException {
-//        isDropping = true;
-//        elbow.setPosition(0);
-//        Thread.sleep(500);
-//        hand.setPosition(0);
-//    }
+    public void armPositionIdle() {
+        isDropping = false;
+        hand.setPosition(.92);
+        clawOpen();
+    }
+
+    public void armPositionDrop() throws InterruptedException {
+        isDropping = true;
+        elbow.setPosition(0);
+        Thread.sleep(500);
+        hand.setPosition(0);
+    }
 
 
-     @Override
+    @Override
     public void runOpMode() throws InterruptedException {
+        telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
         telemetry.addData("Status", "Initialized", "haggis");
 
         // Initialize the hardware variables. Note that the strings used here as parameters
@@ -167,18 +188,23 @@ public class TeleOp2024 extends LinearOpMode {
         specimen = hardwareMap.get(DcMotor.class, "specimen");
         specimen.setDirection(DcMotorSimple.Direction.REVERSE);
         slide.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-      
+
         claw = hardwareMap.get(Servo.class, "grabber");
         rotate = hardwareMap.get(Servo.class,"rotator");
         clawSpecimen = hardwareMap.get(Servo.class, "clawSpecimen");
-        //elbow = hardwareMap.get(Servo.class, "elbow");
-        //hand = hardwareMap.get(Servo.class, "hand");
+        elbow = hardwareMap.get(Servo.class, "elbow");
+        hand = hardwareMap.get(Servo.class, "hand");
 
         //AsyncArmActions armControl = new AsyncArmActions(0.25, this);
+        Gamepad currDriveGamepad = new Gamepad();
+        Gamepad currArmGamepad = new Gamepad();
 
-
+        Gamepad prevDriveGamepad = new Gamepad();
+        Gamepad prevArmGamepad = new Gamepad();
 
         driveGamepad = gamepad1;
+
+        liftController = new MotorController(lift1, lift2, 0.009, 0, 0.0001, 0.05, lastPos);
 
 
         waitForStart();
@@ -186,103 +212,97 @@ public class TeleOp2024 extends LinearOpMode {
 
         // run until the end of the match (driver presses STOP)
         while (opModeIsActive()) {
-           //armControl.moveLiftTo(2400);
+            //armControl.moveLiftTo(2400);
 
-//            if (!positionSet && lift1.getCurrentPosition() > 250) {
-//                positionSet = true;
-//                armPositionIdle();
-//            }
+            if (!positionSet && lift1.getCurrentPosition() > 250) {
+                positionSet = true;
+                armPositionIdle();
+            }
 
-          movement();
+            movement();
 
-          armGamepad = gamepad2.getGamepadId() == -1 ? gamepad1 : gamepad2;
+            armGamepad = gamepad2.getGamepadId() == -1 ? gamepad1 : gamepad2;
 
-          if (!liftRunning) {
-                  if (armGamepad.left_trigger > 0 && (lift1.getCurrentPosition() < 5900 || resetting)) {
-                      liftUp(armGamepad.left_trigger * 0.75);
-                      lastPos = lift1.getCurrentPosition();
-                  } else if (armGamepad.right_trigger > 0) { //&& (lift1.getCurrentPosition() > 230 || resetting)) {
-                      if (lift1.getCurrentPosition() < 400) {
-                          liftDown(armGamepad.right_trigger * 0.75 * Math.max(Math.pow(minLiftPower, Math.abs(lift1.getCurrentPosition() - 0)), minLiftPower));
-                          // f(x) = (total power) * (min power)^(right_trigger)
-                      } else {
-                          liftDown(armGamepad.right_trigger * 0.75);
-                      }
-//                  }
-                      lastPos = lift1.getCurrentPosition();
-                  } else {
-                      int positionError = lastPos - lift1.getCurrentPosition();
-                      double correctionPower;
-                      double kP = 0.25;
+            prevArmGamepad.copy(currArmGamepad);
+            prevDriveGamepad.copy(currDriveGamepad);
 
-                      if (Math.abs(positionError) > 25) {
-                          correctionPower = kP * (positionError / 50.0) * (positionError > 0 ? 1 : -1);
-                          lift1.setPower(correctionPower);
-                          lift2.setPower(correctionPower);
-                      } else {
-                          lift1.setPower(0);
-                          lift2.setPower(0);
-                      }
-                  }
-          }
+            currArmGamepad.copy(armGamepad);
+            currDriveGamepad.copy(driveGamepad);
 
 
+            if (armGamepad.left_trigger > 0 && (lift1.getCurrentPosition() < 5900 || resetting)) {
+                liftUp(armGamepad.left_trigger * 0.75);
+                lastPos = lift1.getCurrentPosition();
+            } else if (armGamepad.right_trigger > 0) { //&& (lift1.getCurrentPosition() > 230 || resetting)) {
+                if (lift1.getCurrentPosition() < 400) {
+                    liftDown(armGamepad.right_trigger * 0.75 * Math.max(Math.pow(minLiftPower, Math.abs(lift1.getCurrentPosition() - 0)), minLiftPower));
+                    // f(x) = (total power) * (min power)^(right_trigger)
+                } else {
+                    liftDown(armGamepad.right_trigger * 0.75);
+                }
+                lastPos = lift1.getCurrentPosition();
+            } else {
+                liftController.setTarget(lastPos);
+                liftController.update();
+            }
 
-//          if (armGamepad.dpad_up) {
-//              armPositionDrop();
-//          }
-//
-//          if (armGamepad.dpad_down) {
-//              armPositionIdle();
-//          }
+            if (armGamepad.dpad_up) {
+                armPositionDrop();
+            }
 
-          if (armGamepad.right_bumper && ((lift1.getCurrentPosition() < 600 && slide.getCurrentPosition() < 2800) || lift1.getCurrentPosition() > 600 || resetting)) {
-              slideUp();
-          } else if (armGamepad.left_bumper) {
-              slideDown();
-          } else {
-              slide.setPower(0);
-          }
+            if (armGamepad.dpad_down) {
+                armPositionIdle();
+            }
 
-          if (driveGamepad.options && driveGamepad.share) {
-              if (resetting) {
-                  lift1.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-                  lift1.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-                  resetting = false;
-              } else {
-                  resetting = true;
-              }
-          }
+            if (armGamepad.right_bumper && (slide.getCurrentPosition() > -2670  || resetting)) {
+                slideUp();
+            } else if (armGamepad.left_bumper) {
+                slideDown();
+            } else {
+                slide.setPower(0);
+            }
 
-          if (armGamepad.triangle) {
-              clawOpen();
-          } else if (armGamepad.cross) {
-              clawClose();
-          }
-          if (armGamepad.dpad_right){
-              rotateClawR();
-          } else if(armGamepad.dpad_left){
-              rotateClawL();
-          }
+            if (driveGamepad.options && driveGamepad.share) {
+                if (resetting) {
+                    lift1.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+                    lift1.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+                    resetting = false;
+                } else {
+                    resetting = true;
+                }
+            }
 
-          if(armGamepad.right_stick_button){
-              specimenUp();
-          } else if(armGamepad.left_stick_button){
-              specimenDown();
-          } else{
-              specimen.setPower(0);
-          }
+            if (armGamepad.triangle) {
+                clawOpen();
+            } else if (armGamepad.cross) {
+                clawClose();
+            }
+            if (currArmGamepad.dpad_right && !prevArmGamepad.dpad_right){
 
-          if(armGamepad.square){
-              claw2Open();
-          } else if(armGamepad.circle){
-              claw2Close();
-          }
+                rotateClawR();
+            } else if(currArmGamepad.dpad_left && !prevArmGamepad.dpad_left){
+                rotateClawL();
+            }
 
-          telemetry.addData("Is Resetting?", resetting);
-          telemetry.addData("Lift Position", lift1.getCurrentPosition());
-          telemetry.addData("Slide Position", slide.getCurrentPosition());
-          telemetry.update();
+            if(armGamepad.right_stick_button){
+                specimenUp();
+            } else if(armGamepad.left_stick_button){
+                specimenDown();
+            } else{
+                specimen.setPower(0);
+            }
+
+            if(armGamepad.square){
+                claw2Open();
+            } else if(armGamepad.circle){
+                claw2Close();
+            }
+
+            telemetry.addData("Is Resetting?", resetting);
+            telemetry.addData("Lift Position", lastPos);
+            telemetry.addData("Target", target);
+            telemetry.addData("rotate index", rotateIndex);
+            telemetry.update();
         }
     }
 }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/MotorController.java
@@ -0,0 +1,39 @@
+package org.firstinspires.ftc.teamcode.utils;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+public class MotorController {
+    private PIDController pid;
+    private double p = 0, i = 0, d = 0;
+    private double ff = 0;
+    private int target = 0;
+    private final double ticks = 384.5; // for 435 rpm motors
+    public DcMotor motor1, motor2;
+    public boolean enabled = true;
+
+    public MotorController(DcMotor motor1, DcMotor motor2, double p, double i, double d, double f, int target) {
+        this.pid = new PIDController(p, i, d);
+        this.ff = Math.cos(Math.toRadians(target/ticks)) * f;
+        this.motor1 = motor1;
+        this.motor2 = motor2;
+        this.target = target;
+    }
+
+    public MotorController(DcMotor motor1, double p, double i, double d, double f, int target) {
+        this(motor1, null, p, i, d, f, target);
+    }
+
+    public void update() {
+        if (enabled) {
+            double power = pid.calculate(motor1.getCurrentPosition(), target);
+            motor1.setPower(power);
+            if (motor2 != null) {
+                motor2.setPower(power);
+            }
+        }
+    }
+
+    public void setTarget(int target) {
+        this.target = target;
+    }
+}
